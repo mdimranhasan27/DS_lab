ff55feb89438476c76dd0721e2760728baed5068
imran <mdimranhasan2023@gmail.com>
2026-04-17 21:00:25 +0600

    DS lab practice

---
+++ Graph/floyd_warshal.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    const int INF = 1e9;
+
+    vector<vector<int>> dist(n, vector<int>(n));
+
+    // input adjacency matrix (use INF if no edge)
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> dist[i][j];
+
+            if ( dist[i][j] == 0)
+                dist[i][j] = INF;
+        }
+    }
+
+    // Floyd–Warshall
+    for (int k = 0; k < n; k++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    // output
+    cout << "\nShortest path matrix:\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (dist[i][j] == INF)
+                cout << "INF ";
+            else
+                cout << dist[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
---
+++ Graph/path_length.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Matrix multiplication
+vector<vector<int>> multiply(vector<vector<int>> A, vector<vector<int>> B, int v)
+{
+    vector<vector<int>> C(v, vector<int>(v, 0));
+
+    for (int i = 0; i < v; i++)
+    {
+        for (int j = 0; j < v; j++)
+        {
+            for (int k = 0; k < v; k++)
+            {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    return C;
+}
+
+// Print matrix
+void print(vector<vector<int>> M, int v)
+{
+    for (int i = 0; i < v; i++)
+    {
+        for (int j = 0; j < v; j++)
+            cout << M[i][j] << " ";
+        cout << "\n";
+    }
+}
+void print2(vector<vector<int>> M, int v)
+{
+    cout << "\nPath length 2:\n";
+    cout << "_From_    _ To_   _number_  \n";
+    for (int i = 0; i < v; i++)
+    {
+
+        for (int j = 0; j < v; j++)
+        {
+            cout << "V" << i << " TO " << "V" << j <<" "<< M[i][j] << "\n ";
+            ;
+        }
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    int v;
+    cin >> v;
+
+    vector<vector<int>> A(v, vector<int>(v));
+
+    // Input adjacency matrix
+    for (int i = 0; i < v; i++)
+        for (int j = 0; j < v; j++)
+            cin >> A[i][j];
+
+    // (a) Adjacent vertices
+    cout << "\nAdjacency list:\n";
+   
+     for (int i = 0; i < v; i++)
+    {
+        cout << "V" << i << ": ";
+        for (int j = 0; j < v; j++)
+        {
+            if (A[i][j] == 1)
+                cout << "V" << j << " ";
+        }
+        cout << "\n";
+    }
+
+    // (b), (c), (d)
+    vector<vector<int>> A2 = multiply(A, A, v);
+    vector<vector<int>> A3 = multiply(A2, A, v);
+    vector<vector<int>> A4 = multiply(A3, A, v);
+
+    cout << "\nPaths of length 2:\n";
+    print2(A2, v);
+
+    cout << "\nPaths of length 3:\n";
+    print(A3, v);
+
+    cout << "\nPaths of length 4:\n";
+    print(A4, v);
+
+    return 0;
+}
